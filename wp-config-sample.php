7112f4e7f7dc1ea519c556d40a5ff2468c6d3f58
Alex <Chavet>
2014-10-09 13:52:45 +1100

    Creating the config file (hidden)

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -16,13 +16,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'database_name_here');
+define('DB_NAME', 'free');
 
 /** MySQL database username */
-define('DB_USER', 'username_here');
+define('DB_USER', 'free');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'password_here');
+define('DB_PASSWORD', 'free');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
