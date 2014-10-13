0a347f2692c6fe5298ecf01b09b96556c3288ccb
Alex <Chavet>
2014-10-13 15:30:42 +1100

    Templating One page layout

---
+++ wp-content/themes/fallengiants/find-us.php
@@ -0,0 +1,31 @@
+<?php
+/**
+
+Template Name: Find us
+ */
+
+?>
+
+<div class="row">
+	<div id="primary" class="site-content small-12 medium-12 columns">
+		<div id="content" role="main">
+
+			
+
+				<article id="<?php echo $post->post_name; ?>" <?php post_class(); ?>>
+
+					<header class="entry-header">
+						<h1 class="entry-title"><?php the_title(); ?></h1>
+					</header>
+
+					<div class="entry-content">
+						<?php the_content(); ?>
+					</div>
+
+				</article>
+
+		</div>
+	</div>
+
+</div>
+
---
+++ wp-content/themes/fallengiants/our-story.php
@@ -0,0 +1,31 @@
+<?php
+/**
+
+Template Name: Our story
+
+ */
+
+?>
+
+<div class="row">
+	<div id="primary" class="site-content small-12 medium-12 columns">
+		<div id="content" role="main">
+
+			
+
+				<article id="<?php echo $post->post_name; ?>" <?php post_class(); ?>>
+
+					<header class="entry-header">
+						<h1 class="entry-title"><?php the_title(); ?></h1>
+					</header>
+					<div class="row">
+						<div class="entry-content large-6 medium-6 small-12 medium-centered columns">
+							<?php the_content(); ?>
+						</div>
+					</div>
+				</article>
+
+		</div>
+	</div>
+
+</div>
