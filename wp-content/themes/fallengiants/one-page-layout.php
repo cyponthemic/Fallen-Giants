d034f605a12acede6ebca90cca4e703dd504aa03
Alex <Chavet>
2014-10-13 15:31:16 +1100

    Template one page layout II

---
+++ wp-content/themes/fallengiants/one-page-layout.php
@@ -0,0 +1,37 @@
+<?php
+/**
+
+Template Name: One page layout
+
+ */
+
+get_header(); ?>
+
+<div class="row">
+	<div id="primary" class="site-content small-12 medium-12 columns">
+	
+		<div id="content" role="main">
+		
+		<?php 
+		global $post;
+                $args = array(
+                    'post_type' => 'page',
+                    'order' => 'ASC'
+                );
+                $the_query = new WP_Query( $args );         
+            ?>
+            <?php if ( have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post(); ?> 
+
+            <?php 
+            
+            get_template_part($post->post_name); ?>
+
+            <?php endwhile; endif; ?>
+            
+		</div>
+	</div>
+
+
+</div>
+
+<?php get_footer(); ?>
